2a3370c80b024a7ba2bee482c56f9295e7330dc2
Keith koki <kokikeith@gmail.com>
2023-05-25 06:33:29 -0700

    updated monty

--- monty.h
+++ monty.h
@@ -42,7 +42,7 @@ typedef struct instruction_s
 void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **stack);
-void pop(stack_t **stack, unsigned int line);
-void pint(stack_t **stack, unsigned int line);
+void pop(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif
