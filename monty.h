e88ab1d802f0ca2cb84d3dacb1f8f1779a70b113
Keith koki <kokikeith@gmail.com>
2023-05-25 12:10:59 -0700

    monty.h updated

--- monty.h
+++ monty.h
@@ -12,6 +12,24 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct arg_s
+{
+	int arg;
+	int flag;
+} arg_t;
+
+extern arg_t arg;
+
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
