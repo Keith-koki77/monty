999a5b75eee3e28daabd11c80aa59b067f10cc9c
ofafax <philipofafa@gmail.com>
2023-05-25 06:38:35 -0700

    changes to pop.c and pint.c"

--- pint.c
+++ pint.c
@@ -3,14 +3,14 @@
 /**
  * pint - prints the element at the top of the stack
  * @stack: double pointer to the head of the stack
- * @line: line number of file
+ * @line_number: line number of file
  */
 
-void pint(stack_t **stack, unsigned int line)
+void pint(stack_t **stack, unsigned int line_number)
 {
 	if (!(*stack))
     {
-		fprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n", line);
+		fprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
     }
 	printf("%d\n", (*stack)->n);
--- pop.c
+++ pop.c
@@ -3,16 +3,16 @@
 /**
  * pop - removes the top element of the stack
  * @stack- double pointer to the top of the stack
- * @line- line number of file
+ * @line_number- line number of file
  */
 
-void pop(stack_t **stack, unsigned int line)
+void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
 
 	if (!(*stack))
 	{
-		dprintf(STDERR_FILENO, "L%u: can't pop an empty stack\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
